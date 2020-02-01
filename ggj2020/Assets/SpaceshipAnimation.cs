b40e7440f09c5b8d094ea1e9eff028d27df32571
Adam Bodnar <adam.p.bodnar@gmail.com>
2020-02-01 11:32:02 -0800

    hurrrrrrrr thrusters

---
+++ ggj2020/Assets/SpaceshipAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipAnimation : MonoBehaviour
+{
+    struct CornerTruster
+    {
+        Vector3 pos;
+        Vector3 dirU;
+        Vector3 dirV;
+        bool toggleU;
+        bool toggleV;
+    };
+
+    Rigidbody rb;
+    Transform t;
+    CornerTruster[] thursters = { 
+        new CornerTruster(),
+        new CornerTruster(),
+        new CornerTruster(),
+        new CornerTruster(),
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForceAtPosition();
+    }
+}
---
+++ ggj2020/Assets/Thruster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour
+{
+    public Rigidbody rb;
+    public KeyCode togglekey;
+    Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(togglekey))
+        {
+            rb.AddForceAtPosition(t.forward * Time.deltaTime * 100, t.position);
+        }
+    }
+}
